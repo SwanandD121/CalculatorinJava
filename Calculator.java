638c89e642554692ed8ad869de8944421ae20e4f
Swanand Deshpande <swananddeshpande121@gmail.com>
2023-05-15 11:56:35 +0530

    Calculator Program

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int ans = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true) {
+            System.out.println("Enter Operation to b performed: ");
+            char operation = scanner.next().trim().charAt(0);
+            if(operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '%') {
+                System.out.println("Enter 2 numbers: ");
+                int num1 = scanner.nextInt();
+                int num2 = scanner.nextInt();
+
+                if(operation == '+'){
+                    ans = num1 + num2;
+                }
+                if(operation == '-'){
+                    ans = num1 - num2;
+                }
+                if(operation == '*'){
+                    ans = num1 * num2;
+                }
+                if(operation == '/'){
+                    if(num2 != 0){
+                        ans = num1 / num2;
+                    }
+                }
+                if(operation == '%'){
+                    ans = num1 % num2;
+                }
+            }
+            else if(operation == 'X' || operation == 'x'){
+                break;
+            }
+            else {
+                System.out.println("Invalid Input");
+            }
+            System.out.println(ans);
+        }
+    }
+}
